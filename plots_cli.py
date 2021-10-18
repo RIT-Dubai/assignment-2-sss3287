671e608ba0bfdbb6873a7fc22b09090dffe85ada
sss3287 <sss3287@rit.edu>
2021-10-18 19:41:57 +0400

    Added function help!

--- plots_cli.py
+++ plots_cli.py
@@ -12,15 +12,29 @@ def quit(x):
         return False
 
 
+
+
+
+def help():
+    print ("stu <filename> <first name> <last name> - plot student grades\n")
+    print ("cavg <filename> - plot class average\n")
+    print ("avg <filename> <number> - prints the average for the grade item\n")
+    print ("quit - quits\n")
+    print ("help - displays this message\n")
+
+
 if __name__ == '__main__':
     try:
         z = raw_input("Enter a Command!")
         c = z.split()
         q = ('quit')
+        h = ('help')
         if c[0] == q:
             quit(raw_input("Are you sure?(y/n)"))
+        elif c[0] == h:
+            help()
         else:
-            raise ("Invalid Command")
+            raise "Invalid Command"
     except:
         print("Enter a command or 'quit' to quit.")
         z = raw_input("Enter a Command!")
@@ -29,7 +43,7 @@ if __name__ == '__main__':
         if c[0] == q:
             quit(raw_input("Are you sure?(y/n)"))
         else:
-            raise ("Invalid Command")
+            raise "Invalid Command"
 
 #quit(raw_input("Are you sure?(y/n)"))
  #or each_cmd in z:
