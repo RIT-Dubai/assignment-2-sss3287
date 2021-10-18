220e88314108700d375e52bf0261545b4d54c14e
sss3287 <sss3287@rit.edu>
2021-10-18 17:21:41 +0400

    Few More modifications done in main function! Looks better now!

--- plots_cli.py
+++ plots_cli.py
@@ -13,15 +13,23 @@ def quit(x):
 
 
 if __name__ == '__main__':
-    z = raw_input("Enter a Command!")
-    c = z.split()
-    q = ('quit')
-try:
-    if c[0] == q:
-        quit(raw_input("Are you sure?(y/n)"))
-    else:
-        raise ("Invalid Command")
-except:
-    print("Enter a command or 'quit' to quit.")
+    try:
+        z = raw_input("Enter a Command!")
+        c = z.split()
+        q = ('quit')
+        if c[0] == q:
+            quit(raw_input("Are you sure?(y/n)"))
+        else:
+            raise ("Invalid Command")
+    except:
+        print("Enter a command or 'quit' to quit.")
+        z = raw_input("Enter a Command!")
+        c = z.split()
+        q = ('quit')
+        if c[0] == q:
+            quit(raw_input("Are you sure?(y/n)"))
+        else:
+            raise ("Invalid Command")
+
 #quit(raw_input("Are you sure?(y/n)"))
  #or each_cmd in z:
