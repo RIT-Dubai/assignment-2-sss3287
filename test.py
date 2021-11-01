f239e97396930eafff0ac49146ec050ac8d298c7
sss3287 <sss3287@rit.edu>
2021-11-01 17:18:59 +0400

    modifications in tests

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ import turtle
 import plots_cli
 
 def test_student_average():
-    a=plots_cli.student_average("stu /Users/sohan_saimbhi/Desktop/GCIS.123.600-assignment2-sample.csv Zeyad Bonita".split())
+    a=plots_cli.student_average("stu GCIS.123.600-assignment2-sample.csv Zeyad Bonita".split())
     assert(a==True)
 
 
 def test_print_average():
-    a=plots_cli.print_average("avg /Users/sohan_saimbhi/Desktop/GCIS.123.600-assignment2-sample.csv 5".split())
+    a=plots_cli.print_average("avg GCIS.123.600-assignment2-sample.csv 5".split())
     assert(a!=-1)
 
 
 def test_class_average():
-    a=plots_cli.class_average("cavg /Users/sohan_saimbhi/Desktop/GCIS.123.600-assignment2-sample.csv".split())
+    a=plots_cli.class_average("cavg GCIS.123.600-assignment2-sample.csv".split())
     assert(a!=None)
